43953acee9fe0d4794175017b4a1f9a480899a1d
Andrey Kolihanov <akpysec@gmail.com>
2021-10-04 00:36:35 +0300

    Update RuleBaseAudit.py

--- RuleBaseAudit.py
+++ RuleBaseAudit.py
@@ -1,52 +1,61 @@
-import sys
 from fw_parsers import fortigate, tufin
 from engine.check_engine import any_srv, any_dst, any_src, disabled, track, worst
+import argparse
 
-arguments = sys.argv
+parser = argparse.ArgumentParser()
+parser.add_argument('-P', '--path',
+                    required=True,
+                    default=None,
+                    dest='path',
+                    help='Provide a Path to a folder with exported IPv4 policy file/s',
+                    type=str
+                    )
 
-checks_functions = [
-    any_src,
-    any_dst,
-    any_srv,
-    disabled,
-    track,
-    worst
-]
+parser.add_argument('-F', '--file-extension',
+                    required=True,
+                    default=None,
+                    dest='file_extension',
+                    help='Provide a file/s extension type',
+                    type=str
+                    )
 
-must_have_args = [
-    '--path',
-    '--file-extension',
-    '--fw-vendor'
-]
+parser.add_argument('-R', '--rule-base-provider',
+                    required=True,
+                    default=None,
+                    dest='rule_base_provider',
+                    help='Provide a Firewall rule base provider name (Tufin, Checkpoint, Palo-Alto, FortiGate)',
+                    type=str
+                    )
 
-optional_args = [
-    '--help',
-    '--encoding-files'
-]
+parser.add_argument('-E', '--encoding',
+                    default='windows-1256',
+                    dest='encoding',
+                    help='Provide a file/s encoding',
+                    type=str
+                    )
 
-for m_args, o_args in zip(must_have_args, optional_args):
-    if m_args not in arguments:
-        print(f'Missing argument "{m_args}"')
-    else:
-        if arguments[6] == 'fortigate':
-            for check in checks_functions:
-                check(fortigate.files_reader(
-                    path_to_files=arguments[2],
-                    file_extension=arguments[4]
+args = parser.parse_args()
+
+if args.rule_base_provider == 'fortigate':
+    any_src(
+        dataframe=fortigate.create_df(
+            rule_base_as_nested_dict=fortigate.rule_base_parsing(
+                raw_dataframe=fortigate.files_reader(
+                    path_to_files=args.path,
+                    file_extension=args.file_extension,
+                    encoding_files=args.encoding
                 )
-                )
-        if arguments[6] == 'tufin':
-            for check in checks_functions:
-                check(tufin.files_reader_and_parser(
-                    path_to_files=arguments[2],
-                    file_extension=arguments[4]
-                )
-                )
-    if o_args == optional_args[0]:
-        print('Required arguments:\n')
-        print(must_have_args[0], '\tchoose path to folder with exported IPv4 policies')
-        print(must_have_args[1], '\tchoose right extension of a file "txt/csv"')
-        print(must_have_args[2], '\tchoose firewall vendor')
-        print('Optional arguments:\n')
-        print(optional_args[0], '\thelp - present options')
-        print(optional_args[1], '\tselect encoding for csv')
+            )
+        )
+    ).to_csv("C:\\Users\\andreyk\\PycharmProjects\\RuleBaseAudit\\any_source.csv")
+
+elif args.rule_base_provider == 'tufin':
+    any_src(
+        dataframe=tufin.files_reader_and_parser(
+            path_to_files=args.path,
+            file_extension=args.file_extension,
+            encoding_files=args.encoding
+        )
+    ).to_csv("C:\\Users\\andreyk\\PycharmProjects\\RuleBaseAudit\\any_source.csv")
+
+
