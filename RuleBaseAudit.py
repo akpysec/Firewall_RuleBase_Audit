07f67f3e2d24446d594df900f1d7b6ca3e334610
akpysec <andriykolihanov@gmail.com>
2021-10-03 19:15:40 +0300

    Un-done, in progress 5 requirements.txt added

--- RuleBaseAudit.py
+++ RuleBaseAudit.py
@@ -1,7 +1,17 @@
 import sys
 from fw_parsers import fortigate, tufin
+from engine.check_engine import any_srv, any_dst, any_src, disabled, track, worst
 
 arguments = sys.argv
+
+checks_functions = [
+    any_src,
+    any_dst,
+    any_srv,
+    disabled,
+    track,
+    worst
+]
 
 must_have_args = [
     '--path',
@@ -14,16 +24,16 @@ for args in must_have_args:
         print(f'Missing argument "{args}"')
     else:
         if arguments[6] == 'fortigate':
-            print(fortigate.files_reader(
-                path_to_files=arguments[2],
-                file_extension=arguments[4]
+            for check in checks_functions:
+                check(fortigate.files_reader(
+                    path_to_files=arguments[2],
+                    file_extension=arguments[4]
                 )
-            )
+                )
         if arguments[6] == 'tufin':
-            print(tufin.files_reader_and_parser(
-                path_to_files=arguments[2],
-                file_extension=arguments[4]
+            for check in checks_functions:
+                check(tufin.files_reader_and_parser(
+                    path_to_files=arguments[2],
+                    file_extension=arguments[4]
                 )
-            )
-
-
+                )
