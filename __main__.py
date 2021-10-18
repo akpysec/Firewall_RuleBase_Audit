ab3594647308bbf9d353585a923427aa883a2576
akpysec <andriykolihanov@gmail.com>
2021-10-18 16:51:42 +0300

    html report generator functionality is added

--- __main__.py
+++ __main__.py
@@ -3,11 +3,12 @@ from policy_providers import fortigate, tufin
 from engine.check_engine import any_srv, any_dst, any_src, disabled, track_logs, worst_rules, crossed_rules
 from engine.cli_flags import args
 from engine.bar_chart import stats_chart
-from output.report_generator import write_report
 
 # Packages Import
 import colored
 from colored import fg, attr, stylize
+import dominate
+from dominate.tags import *
 
 
 BOLD_RED = colored.fg("red") + colored.attr("bold")
@@ -18,51 +19,104 @@ checks = [
     any_srv, any_dst, any_src, disabled, track_logs, worst_rules, crossed_rules
 ]
 
-for check in checks:
-    findings = list() # Put here what found and then write to excel each
-    if args.policy_provider == 'fortigate':
-        forti = check(
-            dataframe=fortigate.create_df(
-                rule_base_as_nested_dict=fortigate.rule_base_parsing(
-                    raw_dataframe=fortigate.files_reader(
+
+with open('Audit report.html', 'a') as htmlsky:
+    doc = dominate.document(title='Audit Report')
+    with doc.head:
+        meta(name="viewport", content="width=device-width, initial-scale=1")
+        style(
+            """
+            .accordion {
+              background-color: #eee;
+              color: #444;
+              cursor: pointer;
+              padding: 18px;
+              width: 100%;
+              border: none;
+              text-align: left;
+              outline: none;
+              font-size: 15px;
+              transition: 0.4s;
+            }
+            
+            .active, .accordion:hover {
+              background-color: #ccc;
+            }
+            
+            .panel {
+              padding: 0 18px;
+              display: none;
+              background-color: white;
+              overflow: hidden;
+            }
+            """
+        )
+    with doc:
+        h2('Findings')
+        for check in checks:
+            findings = list() # Put here what found and then write to excel each
+            if args.policy_provider == 'fortigate':
+                forti = check(
+                    dataframe=fortigate.create_df(
+                        rule_base_as_nested_dict=fortigate.rule_base_parsing(
+                            raw_dataframe=fortigate.files_reader(
+                                path_to_files=args.path,
+                                file_extension=args.file_extension,
+                                encoding_files=args.encoding
+                            )
+                        )
+                    )
+                )
+                if forti is not None:
+                    """
+                    for r in forti.index:
+                        print(forti.iloc[r], forti.columns[0:])"""
+
+                    button(_class="accordion").add(check.__name__.upper().replace('_', ' '))
+                    div(_class="panel").add(p(forti))
+
+                    print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
+                elif forti is None:
+                    print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
+                else:
+                    print(stylize("Something else happened", BOLD_ORANGE))
+
+            elif args.policy_provider == 'tufin':
+                tufi = check(
+                    dataframe=tufin.files_reader_and_parser(
                         path_to_files=args.path,
                         file_extension=args.file_extension,
                         encoding_files=args.encoding
                     )
                 )
-            )
-        )
-        if forti is not None:
-            write_report(
-                path=f"{args.path}\\{args.policy_provider}-Audit-Report.html",
-                accordion_name=check.__name__.upper(),
-                accordion_context=forti.columns
-            )
-            print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
-        elif forti is None:
-            print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
-        else:
-            print(stylize("Something else happened", BOLD_ORANGE))
+                if tufi is not None:
+                    button(_class="accordion").add(check.__name__.upper().replace('_', ' '))
+                    div(_class="panel").add(p(tufi.to_string()))
 
-    elif args.policy_provider == 'tufin':
-        tufi = check(
-            dataframe=tufin.files_reader_and_parser(
-                path_to_files=args.path,
-                file_extension=args.file_extension,
-                encoding_files=args.encoding
-            )
-        )
-        if tufi is not None:
-            write_report(
-                path=f"{args.path}\\{args.policy_provider}-Audit-Report.html",
-                accordion_name=check.__name__.upper(),
-                accordion_context=tufi.columns
-            )
-            print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
-        elif tufi is None:
-            print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
-        else:
-            print(stylize("Something else happened", BOLD_ORANGE))
+                    print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
+                elif tufi is None:
+                    print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
+                else:
+                    print(stylize("Something else happened", BOLD_ORANGE))
+
+        script("""
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+        
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            } else {
+              panel.style.display = "block";
+            }
+          });
+        }
+        """)
+    doc = str(doc).replace('&quot;', '"').replace('&lt;', '<')
+    htmlsky.write(doc)
 
 
 
