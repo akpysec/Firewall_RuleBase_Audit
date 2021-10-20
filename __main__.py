897f5377dd1b1fc7d56699bca396106d1d9a1b10
akpysec <andriykolihanov@gmail.com>
2021-10-20 20:50:59 +0300

    Generated a .xlsx report

--- __main__.py
+++ __main__.py
@@ -7,9 +7,9 @@ from engine.bar_chart import stats_chart
 # Packages Import
 import colored
 from colored import fg, attr, stylize
-import dominate
-from dominate.tags import *
-
+import pandas as pd
+import openpyxl
+import datetime
 
 BOLD_RED = colored.fg("red") + colored.attr("bold")
 BOLD_GREEN = colored.fg("green") + colored.attr("bold")
@@ -19,104 +19,57 @@ checks = [
     any_srv, any_dst, any_src, disabled, track_logs, worst_rules, crossed_rules
 ]
 
+AUDIT_OUTPUT = 'Audit-Checks.xlsx'
+# Couldn't find a way to remove the first sheet('Sheet1'), so I added some lame info
+writer = pd.ExcelWriter(AUDIT_OUTPUT, engine='xlsxwriter')
+base_info = {
+    'Date': [datetime.datetime.today()],
+    'Firewall Type': [args.policy_provider.upper()],
+    'Policy Path': [args.path]
+}
+base_frame = pd.DataFrame(base_info)
+base_frame.to_excel(writer, sheet_name="Base Info", index=False)
+writer.close()
 
-with open('Audit report.html', 'a') as htmlsky:
-    doc = dominate.document(title='Audit Report')
-    with doc.head:
-        meta(name="viewport", content="width=device-width, initial-scale=1")
-        style(
-            """
-            .accordion {
-              background-color: #eee;
-              color: #444;
-              cursor: pointer;
-              padding: 18px;
-              width: 100%;
-              border: none;
-              text-align: left;
-              outline: none;
-              font-size: 15px;
-              transition: 0.4s;
-            }
-            
-            .active, .accordion:hover {
-              background-color: #ccc;
-            }
-            
-            .panel {
-              padding: 0 18px;
-              display: none;
-              background-color: white;
-              overflow: hidden;
-            }
-            """
-        )
-    with doc:
-        h2('Findings')
-        for check in checks:
-            findings = list() # Put here what found and then write to excel each
-            if args.policy_provider == 'fortigate':
-                forti = check(
-                    dataframe=fortigate.create_df(
-                        rule_base_as_nested_dict=fortigate.rule_base_parsing(
-                            raw_dataframe=fortigate.files_reader(
-                                path_to_files=args.path,
-                                file_extension=args.file_extension,
-                                encoding_files=args.encoding
-                            )
-                        )
-                    )
-                )
-                if forti is not None:
-                    """
-                    for r in forti.index:
-                        print(forti.iloc[r], forti.columns[0:])"""
-
-                    button(_class="accordion").add(check.__name__.upper().replace('_', ' '))
-                    div(_class="panel").add(p(forti))
-
-                    print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
-                elif forti is None:
-                    print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
-                else:
-                    print(stylize("Something else happened", BOLD_ORANGE))
-
-            elif args.policy_provider == 'tufin':
-                tufi = check(
-                    dataframe=tufin.files_reader_and_parser(
+for check in checks:
+    findings = list() # Put here what found and then write to excel each
+    if args.policy_provider == 'fortigate':
+        forti = check(
+            dataframe=fortigate.create_df(
+                rule_base_as_nested_dict=fortigate.rule_base_parsing(
+                    raw_dataframe=fortigate.files_reader(
                         path_to_files=args.path,
                         file_extension=args.file_extension,
                         encoding_files=args.encoding
                     )
                 )
-                if tufi is not None:
-                    button(_class="accordion").add(check.__name__.upper().replace('_', ' '))
-                    div(_class="panel").add(p(tufi.to_string()))
+            )
+        )
+        if forti is not None:
+            with pd.ExcelWriter(AUDIT_OUTPUT, mode='a', engine='openpyxl') as writer:
+                forti.to_excel(writer, sheet_name=check.__name__.upper().replace('_', ' '), index=False)
+            print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
+        elif forti is None:
+            print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
+        else:
+            print(stylize("Something else happened", BOLD_ORANGE))
 
-                    print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
-                elif tufi is None:
-                    print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
-                else:
-                    print(stylize("Something else happened", BOLD_ORANGE))
-
-        script("""
-        var acc = document.getElementsByClassName("accordion");
-        var i;
-        
-        for (i = 0; i < acc.length; i++) {
-          acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-              panel.style.display = "none";
-            } else {
-              panel.style.display = "block";
-            }
-          });
-        }
-        """)
-    doc = str(doc).replace('&quot;', '"').replace('&lt;', '<')
-    htmlsky.write(doc)
+    elif args.policy_provider == 'tufin':
+        tufi = check(
+            dataframe=tufin.files_reader_and_parser(
+                path_to_files=args.path,
+                file_extension=args.file_extension,
+                encoding_files=args.encoding
+            )
+        )
+        if tufi is not None:
+            with pd.ExcelWriter(AUDIT_OUTPUT, mode='a', engine='openpyxl') as writer:
+                tufi.to_excel(writer, sheet_name=check.__name__.upper().replace('_', ' '), index=False)
+            print(stylize(f'{check.__name__.upper()} \tFINDING', BOLD_RED))
+        elif tufi is None:
+            print(stylize(f"{check.__name__.upper()} \tPASS", BOLD_GREEN))
+        else:
+            print(stylize("Something else happened", BOLD_ORANGE))
 
 
 
